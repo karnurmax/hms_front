29e65b7837cb1e490023a7a4ed7c3a7bc5b82e3c
karnurmax <karnurmax@mail.ru>
2022-04-13 16:33:40 +0600

    set buildDir

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      lang: 'kk',
     },
   },
 
@@ -56,21 +56,11 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
+      light: true,
     },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  buildDir: 'dist'
 }

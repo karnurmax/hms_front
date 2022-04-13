8ea7f80095e43622b3200d8a7c4cb392a47ceacd
karnurmax <karnurmax@mail.ru>
2022-04-13 16:34:04 +0600

    main menu: draft state for testing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -7,34 +7,43 @@
       fixed
       app
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <v-expansion-panels>
+        <v-expansion-panel v-for="group in menuItems" :key="group.title">
+          <v-expansion-panel-header>
+            {{ group.title }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in group.children"
+                :key="i"
+                :to="item.to"
+                router
+                exact
+              >
+                <v-list-item-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
@@ -46,7 +55,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
@@ -55,7 +64,7 @@
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -70,16 +79,51 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
+      menuItems: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
+          title: 'Мониторинг жилого фонда',
+          children: [
+            {
+              icon: 'mdi-apps',
+              title: 'Размещение гостей',
+              to: '/',
+            },
+            {
+              icon: 'mdi-chart-bubble',
+              title: 'Inspire',
+              to: '/inspire',
+            },
+          ],
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          title: 'Мониторинг нежилого фонда',
+          children: [
+            {
+              icon: 'mdi-apps',
+              title: 'Welcome',
+              to: '/',
+            },
+            {
+              icon: 'mdi-chart-bubble',
+              title: 'Inspire',
+              to: '/inspire',
+            },
+          ],
+        },
+        {
+          title: 'Гости',
+          children: [
+            {
+              icon: 'mdi-apps',
+              title: 'Welcome',
+              to: '/',
+            },
+            {
+              icon: 'mdi-chart-bubble',
+              title: 'Inspire',
+              to: '/inspire',
+            },
+          ],
         },
       ],
       miniVariant: false,
@@ -90,3 +134,9 @@ export default {
   },
 }
 </script>
+<style scoped>
+.v-expansion-panel--active:not(:first-child),
+.v-expansion-panel--active + .v-expansion-panel {
+  margin-top: 1px;
+}
+</style>

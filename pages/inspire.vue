5ac39a52423321e8bbc93fd9a2ec12d8a686da29
Nitro <nitro_1@gmail.com>
2022-04-19 11:07:32 +0600

    added login page

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,21 +1,50 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+    <div id="login">
+        <h1>Войти</h1>
+        <input v-model="input.username" type="text" placeholder="Логин" class="login"/>
+        <input v-model="input.password" type="password" placeholder="Пароль" class="password"/>
+        <button type="button" v-on:click="login()" class="button">Войти</button>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'InspirePage',
-}
+    export default {
+        name: 'LoginPage',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {}
+        }
+    }
 </script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+    .login {
+      border: 1px solid grey;
+      outline: none;
+      padding: 5px;
+    }
+    .password {
+      border: 1px solid grey;
+      outline: none;
+      padding: 5px;
+    }
+    .button {
+      border: 1px solid grey;
+      padding: 5px;
+      background-color: #c9b826;
+    }
+</style>
